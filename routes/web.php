b4b44325aedd4e2dd2d34766d7e59b17eac4a2bb
Raul Ramirez <ramsanraul11@gmail.com>
2022-03-01 19:11:18 +0100

    Added routes

--- routes/web.php
+++ routes/web.php
@@ -64,6 +64,7 @@ Route::group(['prefix' => 'empresa'], function () {
 
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/fitxa', function (){
     return 'return /fitxa';
 });
