434908bb74e22b00d6bf64d634bc8e062c5b40af
adrian <adria@DESKTOP-PFP3F5A>
2022-03-01 19:14:31 +0100

    View finished almost completely

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,7 +51,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users','regex:/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@sense-son.com/'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
