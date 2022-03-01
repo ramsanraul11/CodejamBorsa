2e9c6a94352c86e6fbab85c1255a3e8f08f0d7e4
Raul Ramirez <ramsanraul11@gmail.com>
2022-03-01 19:03:00 +0100

    Added routes

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,61 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
 Auth::routes();
+/*
+ * Con parametro obligatorios
+Route::get('/param/{id}/{user}',function($id,$user){
+    return 'El id és '. $id . ' i usuari és ' . $user;
+});
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Parametros opcionales
+Route::get('/paramOpcional/{id?}/{user?}',function($id=null,$user=null){
+    if ($id===null){
+        return "Id no especificat!!";
+    }
+    if ($user===null){
+        return "User no especificat!!";
+    }
+    return 'El id és '. $id . ' i usuari és ' . $user;
+});*/
+
+Route::group(['prefix' => 'empresa'], function () {
+    Route::get('/', function (){
+        return 'Pagina de /empresa/';
+    });
+    Route::get('/add', function (){
+        return 'Pagina de add';
+    });
+    Route::get('/edit/{id}', function ($id){
+        return 'Pagina de edit ' . $id;
+    });
+    Route::group(['prefix' => 'oferta'], function () {
+        Route::get('/', function (){
+            return 'PAgina empresa/oferta';
+        });
+        Route::get('/add/{idempresa}',function($idempresa){
+            return 'El id empresa és ' . $idempresa;
+        });
+        Route::get('/edit/{idoferta}',function($idoferta){
+            return 'El id oferta és ' . $idoferta;
+        });
+        Route::get('/enviar', function (){
+            return 'PAgina empresa/oferta/enviar';
+        });
+    });
+});
+
+
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/fitxa', function (){
+    return 'return /fitxa';
+});
+
+Route::get('/estudis/add', function (){
+    return 'return estudis add';
+});
